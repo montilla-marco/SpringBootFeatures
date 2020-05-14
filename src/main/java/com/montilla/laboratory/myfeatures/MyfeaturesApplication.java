69c057c5c04b307d57e8be045d15d97d44f8692b
s1922089 <montilla.marco_ex@scotibank.cl>
2020-05-14 10:09:33 -0400

    first commit

---
+++ src/main/java/com/montilla/laboratory/myfeatures/MyfeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.montilla.laboratory.myfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montilla/laboratory/myfeatures/MyfeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montilla.laboratory.myfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
